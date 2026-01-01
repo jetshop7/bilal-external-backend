09582342fba196d817c86dea1ebef5d05de3345a
jetshop7 <lalaboulala8@gmail.com>
2026-01-01 23:22:05 +0100

    Update memory-chat.js

--- api/memory-chat.js
+++ api/memory-chat.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            query: message,
+            memory_type: "chat",
             limit: 5
           })
         }
